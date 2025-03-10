c2b5055240468ba70fd07602247acadf3dc0fc45
Your Name <you@example.com>
2025-03-10 07:40:27 -0500

    operador and

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -1,10 +1,12 @@
 '''
  operadores logicos
  
- aquellos que operan umicamente 
- con valores booleanos (V,F)
+gi con valores booleanos (V,F)
  acorde con las tablas de verdad
  '''
  # 
 y = not True
 print('el resultado de operar con not es',y)
+#ejemplo 2
+y = True and True
+print('el resultado de operar con and es',y)
